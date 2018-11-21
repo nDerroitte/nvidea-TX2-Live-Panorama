d4ce959cc400d3cf3a0813a33f33ea7b7644534b
mtestouri <mehdi.testouri@student.uliege.be>
2018-11-21 14:11:51 +0100

    fix

--- Part1/panorama.py
+++ Part1/panorama.py
@@ -54,7 +54,6 @@ def cylindricalWarpImages(img1,img2,cam_matrix, scaling_factor, resolution, proj
         cyl_warp = cv2.warpAffine(warp2, transfo, (warp2.shape[1] + abs(int(transfo[0][2])),warp2.shape[0]))
 
         #img1 = img1.reshape(cyl_warp.shape)
-
         #output = cv2.addWeighted(img1, 1, cyl_warp, 1, 0)
 
         output = np.zeros_like(cyl_warp)
@@ -65,10 +64,7 @@ def cylindricalWarpImages(img1,img2,cam_matrix, scaling_factor, resolution, proj
         output[a] = img1[a]
         output[b] = cyl_warp[b]
 
-<<<<<<< HEAD
         
-=======
->>>>>>> 31c6a2963e040b78f2a65f20a162f94d7f488407
         output = cv2.fastNlMeansDenoising(output)
 
         return output, transfo[0][2]
