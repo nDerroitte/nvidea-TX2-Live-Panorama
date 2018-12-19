4924f45290aa97651c173eaf636f86cb4b022643
mtestouri <mehditestouri96@gmail.com>
2018-12-19 07:55:16 +0100

    natan arrete de pleurer

---
+++ Part2/BoxComp.py
@@ -0,0 +1,159 @@
+import sys
+import time
+import math
+import os
+import cv2
+import numpy as np
+from util import *
+
+TOLERENCE = 3
+TOL = TOLERENCE
+np.set_printoptions(threshold=np.nan)
+
+
+def evaluateError(img_nb, path, grp_nb, video_seq,draw= False):
+    file_name = path+"box_{}_{}.txt".format(grp_nb,video_seq)
+    generated_file = path + "generatedbox_img_{}_{}".format(grp_nb,video_seq)+"_{0:0=4d}.txt".format(img_nb)
+    img_name = "img_{}_{}".format(grp_nb,video_seq)+"_{0:0=4d}.jpg".format(img_nb)
+    mask_name = "seg_{}_{}".format(grp_nb,video_seq)+"_{0:0=4d}.png".format(img_nb)
+    true_rects = getRectFromImg(img_name, file_name)
+    generated_rects = getRectFromImg(img_name, generated_file)
+    error = []
+    if draw :
+        frame = cv2.imread(path+img_name)
+    for i in range(len(true_rects)):
+        if i >= len(generated_rects):
+            break
+        if draw:
+            cv2.rectangle(frame,(true_rects[i].top_left.x, true_rects[i].top_left.y),(true_rects[i].bottom_right.x, true_rects[i].bottom_right.y),(0,0,255),1)
+            cv2.rectangle(frame,(generated_rects[i].top_left.x, generated_rects[i].top_left.y),(generated_rects[i].bottom_right.x, generated_rects[i].bottom_right.y),(255,0,0),1)
+
+        e1 = insideError(true_rects[i],generated_rects[i], path+mask_name)
+        e2 = outsideError(true_rects[i],generated_rects[i])
+        error.append(rms((e1,e2)))
+    for i in range(abs(len(true_rects)-len(generated_rects))):
+        error.append(100)
+    total_error = me(error)
+    #print("Total error on image {}: {}".format(img_nb,total_error))
+    if draw :
+        cv2.imshow("Compare",frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    return total_error
+
+def getRectFromImg(img_name, file_name):
+    text_file = open(file_name, "r")
+    lines = text_file.read().split('\n')
+    rect = []
+    for line in lines :
+        list = line.split(',')
+        if img_name == list[0]:
+            pt1 = Point(int(list[1]), int(list[2]))
+            width = int(list[3])
+            height = int(list[4])
+            pt2 = Point(pt1.x+ width, pt1.y + height)
+            rect.append(Rectangle(pt1, pt2))
+        else:
+            continue
+    text_file.close()
+    #if not len(rect) :
+        #print("The image {} is not a reference image and cannot be compare!".format(img_name))
+    return rect
+
+def drawRedLines(file_name):
+    text_file = open(file_name, "r")
+    lines = text_file.read().split('\n')
+    img_name = None
+    for line in lines :
+        list = line.split(',')
+        if img_name == None:
+            img_name = list[0]
+            img = cv2.imread("AnoOut/"+img_name)
+        elif img_name != list[0] :
+            cv2.imshow(img_name,img)
+            key = cv2.waitKey(0)
+            if key & 0xFF == ord("q"):
+                exit()
+            img_name = list[0]
+            img = cv2.imread("AnoOut/"+img_name)
+
+        pt1 = (int(list[1]), int(list[2]))
+        width = int(list[3])
+        height = int(list[4])
+        pt2 = (pt1[0]+ width, pt1[1] + height)
+        cv2.rectangle(img,pt1,pt2,(0,0,255),1)
+
+    cv2.imshow(img_name,img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    text_file.close()
+
+def attenuateError(seg_path, rect):
+    seg = cv2.imread(seg_path, 0)
+    sub_mask = seg[rect.top_left.y : rect.bottom_right.y,rect.top_left.x:rect.bottom_right.x]
+    total_pixel = rect.area
+    white_pixel = np.count_nonzero(sub_mask)
+    p = white_pixel*100/total_pixel
+    if p> 50.0:
+        #print("There is too much important pixel in the inside rectangle missing. No attenuation!")
+        return rect.area
+    else :
+        return total_pixel - ((total_pixel-white_pixel)/2)
+
+
+
+def insideError(true_rect, generated_rect, seg_path):
+    a1 = a2 = a3 = a4 = 0
+    #Rectangle 1
+    if generated_rect.top_left.x > true_rect.top_left.x+TOL and generated_rect.top_left.x<true_rect.bottom_right.x:
+        r1 = Rectangle(Point(true_rect.top_left.x, max(generated_rect.top_left.y,true_rect.top_left.y)), Point(generated_rect.top_left.x,min(generated_rect.bottom_right.y,true_rect.bottom_right.y)))
+        a1 = attenuateError(seg_path, r1)
+    #Rectangle 2
+    if generated_rect.bottom_right.y < true_rect.bottom_right.y -TOL and generated_rect.bottom_right.y > true_rect.top_left.y:
+        r2 = Rectangle(Point(true_rect.top_left.x, generated_rect.bottom_right.y), Point(true_rect.bottom_right.x,true_rect.bottom_right.y))
+        a2 =  attenuateError(seg_path, r2)
+    #Rectangle 3
+    if generated_rect.top_left.y > true_rect.top_left.y +TOL and generated_rect.top_left.y < true_rect.bottom_right.y:
+        r3 = Rectangle(Point(true_rect.top_left.x,true_rect.top_left.y), Point( true_rect.bottom_right.x, generated_rect.top_left.y))
+        a3 =  attenuateError(seg_path, r3)
+    #Rectangle 4
+    if generated_rect.bottom_right.x < true_rect.bottom_right.x -TOL and generated_rect.bottom_right.x > true_rect.top_left.x:
+        r4 = Rectangle(Point(generated_rect.bottom_right.x, max(true_rect.top_left.y, generated_rect.top_left.y)), Point(true_rect.bottom_right.x, min(true_rect.bottom_right.y, generated_rect.bottom_right.y)))
+        a4 =  attenuateError(seg_path, r4)
+    total = a1 + a2 + a3 + a4
+    error =  total*100/ true_rect.area
+    #print("Inside error : {}".format(error))
+    return error
+
+def outsideError(true_rect, generated_rect):
+    a1 = a2 = a3 = a4 = 0
+    #Rectangle 1
+    if generated_rect.top_left.x < true_rect.top_left.x- TOL:
+        r1 = Rectangle(Point(generated_rect.top_left.x, max(generated_rect.top_left.y,true_rect.top_left.y)), Point(true_rect.top_left.x,min(generated_rect.bottom_right.y,true_rect.bottom_right.y)))
+        a1 = r1.area
+    #Rectangle 2
+    if generated_rect.bottom_right.y > true_rect.bottom_right.y + TOL:
+        r2 = Rectangle(Point(generated_rect.top_left.x, true_rect.bottom_right.y), Point(generated_rect.bottom_right.x,generated_rect.bottom_right.y))
+        a2 = r2.area
+    #Rectangle 3
+    if generated_rect.top_left.y < true_rect.top_left.y - TOL:
+        r3 = Rectangle(Point(generated_rect.top_left.x,generated_rect.top_left.y), Point( generated_rect.bottom_right.x, true_rect.top_left.y))
+        a3 = r3.area
+    #Rectangle 4
+    if generated_rect.bottom_right.x > true_rect.bottom_right.x +TOL:
+        r4 = Rectangle(Point(true_rect.bottom_right.x, max(true_rect.top_left.y, generated_rect.top_left.y)), Point(generated_rect.bottom_right.x, min(true_rect.bottom_right.y, generated_rect.bottom_right.y)))
+        a4 = r4.area
+    total = a1 + a2 + a3 + a4
+    error =  total*100/ true_rect.area
+    if error >100.0:
+        error = 100.0
+    #print("Outside error : {}".format(error))
+    return error
+
+def boxComp(boxes, img_nb, grp_nb, video_seq, path):
+    writeInFile( boxes,"img_{}_{}".format(grp_nb,video_seq)+"_{0:0=4d}.jpg".format(img_nb))
+    e = evaluateError(img_nb, path, grp_nb, video_seq)
+    os.remove(path+"generatedbox_img_{}_{}".format(grp_nb,video_seq)+"_{0:0=4d}.txt".format(img_nb))
+    return e
---
+++ Part2/PersonDetection.py
@@ -0,0 +1,135 @@
+from HumanDetectorTF import HumanDetectorTF
+from HumanDetectorOCV import HumanDetectorOCV
+from BoxComp import *
+import os, sys, time
+
+VIDEO_PATH = "Capture/Video_1_2018-12-03_14_42_52/"
+ANN_PATH = "Annotation/In/box_6_1.txt"
+
+GRP_NB = 6
+VIDEO_SEQ = 1
+ANN_FOLDER = "Annotation/In/"
+
+THRESHOLD = 0.8
+MODEL_PATH = "models/ssd_inception_v2_coco_2018_01_28/frozen_inference_graph.pb"
+
+THRESHOLDS = [0.6,0.7,0.8]
+MODELS = [
+    "models/ssdlite_mobilenet_v2_coco_2018_05_09/frozen_inference_graph.pb",
+    "models/ssd_mobilenet_v2_coco_2018_03_29/frozen_inference_graph.pb",
+    "models/ssd_inception_v2_coco_2018_01_28/frozen_inference_graph.pb",
+    "models/faster_rcnn_inception_v2_coco_2018_01_28/frozen_inference_graph.pb"]
+
+def personDetection(algo, img, detector, p_a=False, img_nb=None
+    , threshold = 0.8):
+
+    start = time.time()
+    if algo == 'tensorflow':
+        boxes = detector.detect(img,threshold)
+    if algo == 'opencv':
+        boxes = detector.detect(img)
+    detection_time = time.time() - start
+
+    if p_a:
+        boxes.sort(key=lambda x: x[0][0])
+        error = boxComp(boxes, img_nb, GRP_NB,VIDEO_SEQ, ANN_FOLDER)
+
+    for box in boxes:
+        cv2.rectangle(img,box[0],box[1],(0,0,255),2)
+    cv2.imshow("preview", img)
+
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
+        exit()
+
+    if p_a:
+        return error, detection_time
+    else:
+        return detection_time
+
+def detect_opcv(video_path,ann_path):
+    files = sorted(os.listdir(video_path))
+    ref_img = getRefId(ann_path)
+    detector = HumanDetectorOCV()
+
+    for file_name in files:
+        if file_name.split(".")[-1].lower() in {"jpeg", "jpg", "png"}:
+            image = cv2.imread(video_path + file_name)
+            if image is not None:
+                personDetection("opencv",image,detector)
+
+def detect_tf(video_path,ann_path,model_path,threshold):
+    files = sorted(os.listdir(video_path))
+    ref_img = getRefId(ann_path)
+    detector = HumanDetectorTF(model_path)
+
+    for file_name in files:
+        if file_name.split(".")[-1].lower() in {"jpeg", "jpg", "png"}:
+            image = cv2.imread(video_path + file_name)
+            if image is not None:
+                personDetection("tensorflow",image,detector,threshold=threshold)
+
+def perf_ass_opcv(video_path,ann_path):
+    files = sorted(os.listdir(video_path))
+    ref_img = getRefId(ann_path)
+    detector = HumanDetectorOCV()
+
+    img_nb=0
+    ann_nb=0
+    total_error = 0
+    computation_time = 0
+
+    for file_name in files:
+        if file_name.split(".")[-1].lower() in {"jpeg", "jpg", "png"}:
+            image = cv2.imread(video_path + file_name)
+            if image is not None:
+                if img_nb in ref_img:
+                    error, detection_time = personDetection("opencv",image,
+                        detector,True,img_nb)
+                    total_error += error
+                    ann_nb += 1
+                else:
+                    detection_time = personDetection("opencv",image,detector)
+                computation_time += detection_time
+        img_nb += 1
+    return (total_error/(ann_nb+1)), (computation_time/(img_nb+1))
+
+def perf_ass_tf(video_path, ann_path, model_path, threshold):
+    files = sorted(os.listdir(video_path))
+    ref_img = getRefId(ann_path)
+    detector = HumanDetectorTF(model_path)
+
+    img_nb=0
+    ann_nb=0
+    total_error = 0
+    computation_time = 0
+
+    for file_name in files:
+        if file_name.split(".")[-1].lower() in {"jpeg", "jpg", "png"}:
+            image = cv2.imread(video_path + file_name)
+            if image is not None:
+                if img_nb in ref_img:
+                    error, detection_time = personDetection("opencv",image,
+                        detector,True,img_nb,threshold)
+                    total_error += error
+                    ann_nb += 1
+                else:
+                    detection_time = personDetection("opencv",image,detector
+                        ,threshold=threshold)
+                computation_time += detection_time
+        img_nb += 1
+    return (total_error/(ann_nb+1)), (computation_time/(img_nb+1))
+
+def study_params(video_path, ann_path, models, thresholds):
+    for model_path in models:
+        for threshold in thresholds:
+            error, computation_time = perf_ass_tf(video_path, ann_path
+                , model_path, threshold)
+            print("\nmodel : " + model_path)
+            print("threshold : " + str(threshold))
+            print("error : " + str(error))
+            print("computation time : " + str(computation_time))
+
+if __name__ == "__main__":
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    study_params(VIDEO_PATH,ANN_PATH,MODELS,THRESHOLDS)
