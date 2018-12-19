17e510c7f4dcdc6217e3d1a0cb5900ac4cf3492b
Natan <33931517+nDerroitte@users.noreply.github.com>
2018-12-19 08:35:47 +0100

    Update Util.py

--- Part2/Util.py
+++ Part2/Util.py
@@ -1,5 +1,7 @@
 import math
-
+###############################################################################
+#                                  Classes                                    #
+###############################################################################
 class Rectangle():
     def __init__(self, top_left , bottom_right):
         self.top_left = top_left
@@ -12,8 +14,13 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
+###############################################################################
+#                                  Functions                                  #
+###############################################################################
 def rms (error_list):
+    """
+    Compute RMS of the error contained in a list. Return the RMS value.
+    """
     sum = 0
     for i in error_list:
         sum+= math.pow(i,2)
@@ -22,6 +29,9 @@ def rms (error_list):
     return sum
 
 def me(error_list):
+    """
+    Compute the mean error of tje error contained in a list. Return the ME value
+    """
     if not len(error_list):
         return -1
     sum = 0
@@ -31,9 +41,14 @@ def me(error_list):
     return sum
 
 def writeInFile(boxes, img_name):
+    """
+    Write the boxes contained as ((topleft),(bottomright),...) into a file as a line described
+    in the image annotation part of the statement.
+    """
     file_name = "AnoIn/generatedbox_"+str(img_name)+".txt"
     file = open(file_name,"w")
     for box in boxes:
+        #Transforamtion
         width = box[1][0]- box[0][0]
         height = box[1][1] - box[0][1]
         to_write = str(img_name)+", "+str(box[0][0])+", "+str(box[0][1])+", "+str(width)+", "+str(height+"\n")
@@ -41,9 +56,13 @@ def writeInFile(boxes, img_name):
     file.close()
 
 def getRefId(file_path):
+    """
+    Get the list of image id that are manually label, ie the reference images
+    """
     ref_id = []
     text_file = open(file_path, "r")
     lines = text_file.read().split('\n')
+    #Read all the lines of the file.
     for line in lines :
         list = line.split(',')
         if(list[0]==""):
